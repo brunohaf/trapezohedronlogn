b88655f89fbe3f7e76fdbb1eaa6b403c23f0a612
brunohaf <bruno.hafonseca@gmail.com>
2025-07-27 16:46:13 -0300

    feat: leetcode#3

--- leetcode/string/medium/3_longest_substring_without_repeating_characters.py
+++ leetcode/string/medium/3_longest_substring_without_repeating_characters.py
@@ -15,122 +15,99 @@
 #
 # Given a string s, find the length of the longest substring without duplicate
 # characters.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: s = "abcabcbb"
 # Output: 3
 # Explanation: The answer is "abc", with the length of 3.
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: s = "bbbbb"
 # Output: 1
 # Explanation: The answer is "b", with the length of 1.
-# 
-# 
+#
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: s = "pwwkew"
 # Output: 3
 # Explanation: The answer is "wke", with the length of 3.
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a
 # substring.
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 0 <= s.length <= 5 * 10^4
 # s consists of English letters, digits, symbols and spaces.
-# 
-# 
+#
+#
 #
 
 # @lc code=start
-class Solution:
-    def getSubstringLen(self, s: str, end: int, start: int = 0) -> str:
-        substring = {}
-        for i in range(start, end):
-            c = s[i]
-            if substring.get(c, False): 
-                return len(substring.keys())
-            substring[c] = True
-        return len(substring.keys())
-        
 
-    def lengthOfLongestSubstring(self, s: str) -> int: 
-        if not len(s):
-            return 0
-        # bruteforce: I will consider there's only 1 longest substr.
-        longest_len = 0
-        for i, c in enumerate(s):
-            substr_len = self.getSubstringLen(s, len(s), i)
-            if substr_len > longest_len:
-                longest_len = substr_len
-        return longest_len
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen_chars: set[str] = set()
+        max_len, left = 0, 0
+        for right in range(len(s)):
+            while s[right] in seen_chars:
+                seen_chars.remove(s[left])
+                left += 1
+            seen_chars.add(s[right])
+            max_len = max(max_len, right - left + 1)
+        return max_len
+
 
 # @lc code=end
 
-# sol = Solution()
-# assert sol.lengthOfLongestSubstring("abcabcbb") == 3
-# assert sol.lengthOfLongestSubstring("bbbbb") == 1
-# assert sol.lengthOfLongestSubstring("pwwkew") == 3
-# assert sol.lengthOfLongestSubstring("") == 0
-# assert sol.lengthOfLongestSubstring(" ") == 1
-# assert sol.lengthOfLongestSubstring("          ") == 1
-# print("SUCCESS!")
+sol: Solution = Solution()
+assert sol.lengthOfLongestSubstring(s="") == 0
+assert sol.lengthOfLongestSubstring(s="          ") == 1
+assert sol.lengthOfLongestSubstring(s="bbbbb") == 1
+assert sol.lengthOfLongestSubstring(s="au") == 2
+assert sol.lengthOfLongestSubstring(s="dvdf") == 3
+assert sol.lengthOfLongestSubstring(s="abcabcbb") == 3
 
-## input: 
-## s - string: 0 < len(s) < 50.000 | \w, \d, \s and symbols.
-#
-## longest substring without duplicated chars
-## at least O(n) because I need to check all possibilities
-#
-## subproblems:
-## 1 - iterate s from 0 to len(s)-1.
-## 2 - build one substrings 
-##      - iterate from i storing the chars
-##      - stop if char is already stored.
-##      - save substring and its len.
-## 3 - repeat 2, incrementing i until i == len(s)-1
-#
-## edge caes:
-## len(s) == 0
-## many/all duplicated chars? worst case O(n)
-## more than one longest substring?
-#
-## toolbox:
-## reusable hashmap for subproblem 2.
-## 
-
-'''
+"""
 @notes post-mortem=start
 -----BEGIN PGP MESSAGE-----
 
-hF4DjxTuSjEZ/v8SAQdAIOLtuSJD/eyKNzcPPh2N2WaK0CEglGysL+XRPnpfwwYw
-NulplL60xxLr0aRPUHxPUPf/teuvRPbFSeuHnQiABm7YJmPYEKmoabaZvHafe5wd
-0ukBBirMhBV6aniAuHqSf35m6lGBWl9jnPMobS2hxOvWOFoQMNcMPZll8Gm1Irhe
-duRqzgFslK90dpYMk6iHkgfqfi7E65XfWRtxVStYuCS4/FYNPnGrJJ6cx4BvoUgl
-wOiyJTrACl3ZOyZGxibjoEXR3k4ICa6rGKDEuh9QuPkOOabSX9zjPjNMf1L2svu5
-7p0c1pKZbkgQnm3MFpTy7aSqvkEhUfBIBvSK79P9i5ZTf14hgFNRIkrXMM67fu+L
-0clkmXeoH1+HDd2VJvzrYsfgwfz0rgGBrJyi+x+pVrV+9WwBLEIG06k06B5g8OAz
-TehReo/yUA3/STrxTar+1dQaDefZEWjzMQJpezRp5n0NPZ4FUhuTRwwkXzDWt8c1
-9qr3HJ1kKtA4LezTvnrnzinH8GwR5ph+9AiMXl+I8WI5ksh1WGM8RoCEHqGebc+E
-S5okvlp772AmL+1jNHDt9OfsVemxPAi3AMKqfcv/gcdpK0usdvpoahO6ipk9dU37
-eHLql8nQeFH+g9b9M6gM85qqtFkvq5j59i5O8cOwS2rWMZi67LIR671i6pUQuINd
-3DSwhjsbuGWF+O1f+eEcrI/8Ac+aHOWxWgx4qBGDm7h5P2R6KKUAXZj5hOtegYtY
-kFhQN1Ybg+FIdwE0kLfyzUqdbKPqP7FQxzZQZrJXlGab1YH5M3yTJ5pVDibqn5IX
-DM6HuJg645sNpZ82J58J7UXL8UfvvGv9n4x/dQLynnRy0/qyOIXl6iB7vEd6S2ll
-cuupjoXgaA5pw0LkPQ/1+CQ0woy0jtMbpjJcma89jpvRZFpwbK199Y3aqoHc6v3i
-zmgJFfPnuZOd3cr53oN/jaqJvzY=
-=9Gz5
+hF4DjxTuSjEZ/v8SAQdA5mWDYmC2tUBfsozkl8o5PGsIkuyuSep0zEjcWtZ1xG4w
+zG5hbaiZuadLkE9JkKCPvCATUq5Qkyi1toHjTcsyDJUlMc7pacAfhHCz6n2DM1Hc
+0uoBzlI2PjSIW1v7lh08O7ol76UjzrTDW23ls7LyuH8iVybHfVCEWZcqVowKXy/q
+twNiPqQbySzJUQqioC2KzOYD62LVOLdMWPuAOMh99Ctl54tjq8bUVbTDHNFS5IH7
+MuvcHjJSfVzMDdQyiIPMRZgC7B2fwuusYi3E1Tpr4iCzawaT4/YyZimOJ+s3Xxzm
+tnffQXgsOUAUdDceah156eaLE+57XNxMXWmOe1opliCuY/6pOccHGcMhuueqdqN9
+4VpJFVuf/rZi+NGXQekMxUlpUaagt4mD6gEHs+wBHUIm1NvzcIpCqinY38ZLIoR6
+JippLiNcdnOP3WtESeqmkKd730xBvMaTH7GagECbD64t5wC0D4rqoIYgteZCvyuX
+8p1kkbHTqw85vY9/wVbGhNNwdlD8h4u+k/MWXpDAENtZSso2s8VMwE568YMcubPQ
+Qkm8b/+8RXXQxMV6QvHuKNSCQqIcBQA1A7YgHTiZRwgLC4U42GvzlnsWQ0N7FC0a
+Lg4Ru0yHnz/OiDETycexGtMGT7JsT2PArQ6jRUYU6DFrYovLhOeeJf2Kvvyu4KDg
+aqaic6AAxHMhr3lgGBpgQFr9PR0d7MAj90YuksAAEkYi72E0NAnpMDzxGiT1puIo
+AsE/aY7Eu+A5AG9H5Wt7jQ8PbF7o3/v6RIaXJiOJ3pvzmydjMmt3Ldxt7Kbl5/BS
+mB7ZLafMu5t0PmVSu8+NVw09Tx3dBh2WzAihqgcNFDYv4UG7kUc5/IHV8DojADcr
+8bWKcxnCMnGl5UVf/ZKuYUI/83kYpOhjNRy8Ws973kJoLhkUDI56Qj0qgig32p3m
+TINk1dGK3rA+W2L4p6TK5RdIuwH5Q9myX8Qs8AFBXVkggOtJg+7Ngpyv+nfiUMic
+X9IojvGEFamIWwDRUGHcY9DN0vzOPVrMtehxN3biMSdBgRjHK2eCWEB26Io+aUHW
+0y55juqoGYYvNPoszE9VKLyiHG+wsPptyOunisr8h5gh/QJVNznQjkVpH3SEmAmz
+P6ECZQkX4lJTY90CKt0J5Hf554aKpSyBpbQNfU9nLYMRSMdiZ+rrfdvF4nBNtT3v
+IUCLRBH2FApI2MzaRHPgmPi5DgEZdEN6KrZqJagMe53jZHnfga3XRkun2D507DV6
+/OfyEKk0b/4v5+it3spPiHd/5yoCGrL8+LhOmCnMBFFGzO6iGf3Mq9u/ie+ePJhD
+1+I4E/C3oQZZYVdzbEXsF9evFJ/V0sWE5Hb9luEB5UYJtQWzQ2d1Uq2xmhVa8twQ
+2TPSziIAzoGP6LWXDJRk9QGRiw4mEu2gIIrD7qPJojFDPYCrmhjzdrXQ393m9UQx
+LhRWx6RrRhOpXzrBTRx6eJEIFDp5A/5qwg6r079RyqBCu49RNpB1
+=y9R/
 -----END PGP MESSAGE-----
 @notes post-mortem=end
-'''
+"""
